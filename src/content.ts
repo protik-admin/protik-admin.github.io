9c9aaaf0cabeefb8521ddbabd5e18397a83051f8
Protik Barua <protik_3802095@bscse.puc.ac.bd>
2026-04-10 02:11:01 +0600

    Add content.ts with personal and project details

---
+++ src/content.ts
@@ -0,0 +1,26 @@
+export const content = {
+  name: "Protik",
+  tagline: "Ads • MLOps • Cyber Security",
+  about:
+    "Ads, ML-Ops & Cyber Sec. Interested in people who are passionate about areas of work they're engaged in.",
+  education: [
+    {
+      school: "Premier University",
+      degree: "B.Sc. in Computer Science & Engineering (CSE)"
+    }
+  ],
+  projects: [
+    {
+      title: "Avaan.tech",
+      description:
+        "An Amazon affiliate program site I’ve created.",
+      href: "https://www.avaan.tech",
+      tags: ["Affiliate", "Web", "SEO"]
+    }
+  ],
+  contact: {
+    email: "protik_3802095@bscse.puc.ac.bd",
+    linkedin: "https://www.linkedin.com/in/protik-cops/",
+    github: "https://github.com/protik-admin"
+  }
+};
